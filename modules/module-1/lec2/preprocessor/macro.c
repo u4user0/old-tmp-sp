d2b295980051d2728f186e299f0b2fb519ec6626
singh <107493787+u4user0@users.noreply.github.com>
2022-09-04 14:27:31 +0530

    Add files via upload

---
+++ modules/module-1/lec2/preprocessor/macro.c
@@ -0,0 +1,12 @@
+// This is main.cpp file name macro.cpp
+#define max(a,b) a>b?a:b
+#include "source.h"
+int main() {
+return multiply(5);
+/*
+	int a=64;
+	int b=8;
+//macro expand while preprocessing
+	return max(a,b);
+*/
+}
