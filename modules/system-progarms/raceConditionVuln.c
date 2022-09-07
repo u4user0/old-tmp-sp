07e7d5d56c35d6c1eea802fba3ca681ada667a2f
singh <107493787+u4user0@users.noreply.github.com>
2022-09-07 21:11:28 +0530

    Add files via upload

---
+++ modules/system-progarms/raceConditionVuln.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+int main() {
+/*
+	*   Real use mkdir, change a little bit to use this with openat
+	*   int mkdir(const char *pathname, mode_t mode);
+	*   int mkdirat(int dirfd, const char *pathname, mode_t mode);
+	*   Working code mkdirat.
+
+*/
+	int fd1, fd2,fd3;
+	fd1 = open("../imp/dummy",O_DIRECTORY);
+	printf("Now sleeping for 14 seconds\n");
+	sleep(14);
+	printf("Now 1second left\n");
+	sleep(1);
+	fd2 = mkdirat(fd1,"./test",0777);
+	fd3 = mkdirat(fd1,"./test4",0777);
+/*
+	This code was not working...
+	fd1 = mkdir("../imp/dummy/test",0777);
+	printf("Now sleeping for 14 seconds\n");
+	sleep(14);
+	printf("Now 1second left\n");
+	sleep(1);
+	fd2 = mkdir("../imp/dummy/test1",0777);
+*/
+	return 0;
+}
